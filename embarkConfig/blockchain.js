910884c6d5a77c8e5e6eb9b302990d51362ca4f9
emizzle <eric.mastro@gmail.com>
2019-05-24 08:26:56 +0700

    add CORS for CRA port 3000

--- embarkConfig/blockchain.js
+++ embarkConfig/blockchain.js
@@ -4,10 +4,16 @@ module.exports = {
     enabled: true,
     rpcHost: "localhost", // HTTP-RPC server listening interface (default: "localhost")
     rpcPort: 8545, // HTTP-RPC server listening port (default: 8545)
-    rpcCorsDomain: "auto",  // Comma separated list of domains from which to accept cross origin requests (browser enforced)
+    rpcCorsDomain: {
+      auto: true,
+      additionalCors: ["http://localhost:3000"]
+    },  // Comma separated list of domains from which to accept cross origin requests (browser enforced)
                             // When set to "auto", Embark will automatically set the cors to the address of the webserver
     wsRPC: true, // Enable the WS-RPC server
-    wsOrigins: "auto",  // Origins from which to accept websockets requests
+    wsOrigins: {
+      auto: true,
+      additionalCors: ["http://localhost:3000"]
+    },  // Origins from which to accept websockets requests
                         // When set to "auto", Embark will automatically set the cors to the address of the webserver
     wsHost: "localhost", // WS-RPC server listening interface (default: "localhost")
     wsPort: 8546 // WS-RPC server listening port (default: 8546)
